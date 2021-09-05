38ca37d72474a02c63f5b6dc60444a567b944d0b
dankito <git@dankito.net>
2021-09-06 00:09:33 +0200

    Added logError() overload without Throwable (is better callable from Java)

--- ElasticsearchLoggerBase/src/main/kotlin/net/codinux/log/elasticsearch/errorhandler/ErrorHandler.kt
+++ ElasticsearchLoggerBase/src/main/kotlin/net/codinux/log/elasticsearch/errorhandler/ErrorHandler.kt
@@ -3,6 +3,10 @@ package net.codinux.log.elasticsearch.errorhandler
 
 interface ErrorHandler {
 
-    fun logError(message: String, e: Throwable? = null)
+    fun logError(message: String) {
+        logError(message, null)
+    }
+
+    fun logError(message: String, e: Throwable?)
 
 }
